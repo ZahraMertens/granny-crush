431ec34c37bf45eb139799d1df200190e4666679
rpgarde <rpgarde@gmail.com>
2021-09-01 23:15:54 +1000

    models + seeds work for mvp

---
+++ models/index.js
@@ -0,0 +1,38 @@
+const User = require('./User');
+const Hobby = require('./Hobby');
+const UserHobby = require ('./UserHobby');
+ const UserMatch = require ('./UserMatch');
+
+
+// User and Hobby many to many relationship (through UserHobby)
+User.belongsToMany(Hobby,{
+    through:{
+        model:UserHobby,
+        unique:false,
+    },
+    as:'associated_hobbies'
+})
+
+Hobby.belongsToMany(User,{
+    through:{
+        model:UserHobby,
+        unique:false,
+    },
+    as:'associated_products'
+})
+
+// User and User many to many relationship (through UserHobby)
+// User.belongsToMany(User,{
+//     through:{
+//         model:UserMatch
+//     },
+//     as: 'user_id',
+//     foreignKey: 'id'
+// })
+
+User.belongsToMany(User,{
+    through:UserMatch,
+    as: 'match'
+})
+
+module.exports = { User, Hobby, UserHobby, UserMatch }
---
+++ seeds/seed.js
@@ -0,0 +1,31 @@
+const { User, Hobby , UserHobby , UserMatch } = require('../models');
+
+const userData = require('./userData.json');
+const hobbyData = require('./hobbyData.json');
+const userHobbyData = require('./userHobbyData.json');
+const userMatchData = require('./userMatchData.json');
+
+const sequelize = require('../config/connection');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+  console.log('\n----- DATABASE SYNCED -----\n');
+
+  const user = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  console.log('\n----- USERS SEEDED -----\n');
+  const hobby = await Hobby.bulkCreate(hobbyData)
+  console.log('\n----- HOBBIES SEEDED -----\n');
+
+  const userHobby = await UserHobby.bulkCreate(userHobbyData)
+  console.log('\n----- USER HOBBIES SEEDED -----\n');
+
+  const userMatch = await UserMatch.bulkCreate(userMatchData)
+  console.log('\n----- USER MATCHES SEEDED -----\n');
+
+  process.exit(0);
+};
+
+seedDatabase();
