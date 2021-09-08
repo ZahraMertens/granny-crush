4252dcd100ddfa012d865d04e7105e64a196bf9d
rpgarde <rpgarde@gmail.com>
2021-09-08 19:43:17 +1000

    GET / POST / DELETE routes working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const matchRoutes = require('./matchRoutes')
 
 router.use('/users', userRoutes);
+router.use('/matches', matchRoutes)
 
 module.exports = router;
